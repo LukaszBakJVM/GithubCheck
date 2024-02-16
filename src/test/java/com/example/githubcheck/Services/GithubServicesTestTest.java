661d979d969031c0d1b8c806f4d73dc2088249e5
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-02-16 19:05:01 +0100

    Test

--- src/test/java/com/example/githubcheck/Services/GithubServicesTestTest.java
+++ src/test/java/com/example/githubcheck/Services/GithubServicesTestTest.java
@@ -5,6 +5,7 @@ package com.example.githubcheck.Services;
 
 
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,34 +50,67 @@ public class GithubServicesTestTest {
 
 
 
+
     @Test
     public void testGetUserRepositoriesSuccess()  {
 
 
             webTestClient.options().uri(wireMockServer.baseUrl());
-            System.out.println(wireMockServer.getPort());
 
-             wireMockServer.stubFor(get(urlPathEqualTo("/users/octocat/repos"))
+
+        StubMapping stubMapping = wireMockServer.stubFor(get(urlPathEqualTo("/users/octocat/repos"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+        System.out.println(stubMapping.getResponse());
+
+
+        wireMockServer.stubFor(get(urlPathEqualTo("/repos/octocat/git-consortium/branches"))
                     .willReturn(aResponse()
                             .withStatus(HttpStatus.OK.value())
-                            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+                            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)))
+                .getMetadata();
 
 
-            wireMockServer.stubFor(get(urlPathEqualTo("/repos/octocat/git-consortium/branches"))
-                    .willReturn(aResponse()
-                            .withStatus(HttpStatus.OK.value())
-                            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
-
-
-        webTestClient.get()
+         webTestClient.get()
                 .uri("/repositories/octocat/fork=false")
                 .exchange()
-                .expectStatus().isOk();
-       // assertThat().as("Wrong response status code").isEqualTo(200);
+                .expectStatus().isOk()
+                .expectBody();
+
+
 
 
 
     }
+
+
+    @Test
+    public void testGetUserRepositoriesNotFound()  {
+        String username = "whenUserNotFound";
+        webTestClient.options().uri(wireMockServer.baseUrl());
+        StubMapping stubMapping = wireMockServer.stubFor(get(urlPathEqualTo("/users/"+username+"/repos"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.NOT_FOUND.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)));
+        System.out.println(stubMapping.getRequest());
+
+        webTestClient.get()
+                .uri("/repositories/whenUserNotFound/fork=false")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(String.class)
+                .isEqualTo("User "+username+" not found");
+
+    }
+  /*  @Test
+    public void recordWiremock() throws InterruptedException {
+        System.out.println(wireMockServer.getPort());
+        while (true){
+            Thread.sleep(4000);
+        }*/
+
+
 
 
 
