7a6c31619b7ef314176394ab13e3f2e672324400
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-02-27 18:43:35 +0100

    Dto

--- src/main/java/com/example/githubcheck/Controller/GithubController.java
+++ src/main/java/com/example/githubcheck/Controller/GithubController.java
@@ -2,12 +2,12 @@ package com.example.githubcheck.Controller;
 
 
 import com.example.githubcheck.Dto.RepositoryDto;
-import com.example.githubcheck.Model.Repository;
 import com.example.githubcheck.Services.GithubServices;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
--- src/main/java/com/example/githubcheck/Dto/CommitDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.githubcheck.Dto;
-
-public record CommitDto(String shaDto) {
-}
