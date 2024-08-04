e914eb7e38b9b6fa0ab96afbf6d1227d2e0ef16d
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-08-04 18:31:11 +0200

    Test ExceededLimit

--- src/test/java/com/example/githubcheck/services/GithubServicesTestTest.java
+++ src/test/java/com/example/githubcheck/services/GithubServicesTestTest.java
@@ -5,7 +5,6 @@ import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
@@ -17,7 +16,6 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
 class GithubServicesTestTest {
 
     @LocalServerPort
